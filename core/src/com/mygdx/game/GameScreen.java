40a616c36fec2ec87b6f5dfe00e9272fbf1514ed
Chattiwa Kanarat <Chattiwa Kanarat>
2016-12-07 00:43:12 +0700

    add table to center and add sign in blue/red Block

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -15,8 +15,10 @@ public class GameScreen extends ScreenAdapter {
     private QuickAttacker quickAttacker;
     private Texture bg;
     private Texture blackBlock;
-    private Texture redBlock;
-    private Texture blueBlock;
+    private Texture redBlock_s;
+    private Texture blueBlock_s;
+    private Texture redBlock_d;
+    private Texture blueBlock_d;
     private Texture redTable;
     private Texture blueTable;
     private World world;
@@ -36,8 +38,10 @@ public class GameScreen extends ScreenAdapter {
         this.quickAttacker = quickAttacker;
         bg = new Texture("bg-center.gif");
         blackBlock = new Texture("blackblock.png");
-        redBlock = new Texture("redblock.png");
-        blueBlock = new Texture("blueblock.png");
+        redBlock_s = new Texture("redblock-s.png");
+        blueBlock_s = new Texture("blueblock-s.png");
+        redBlock_d = new Texture("redblock-d.png");
+        blueBlock_d = new Texture("blueblock-d.png");
         redTable = new Texture("redtable.png");
         blueTable = new Texture("bluetable.png");
         world = new World(quickAttacker);
@@ -82,28 +86,28 @@ public class GameScreen extends ScreenAdapter {
         if(gameStage==2) {    
             batch.begin();
             batch.draw(bg, 0, 0);
-            batch.draw(blueTable, 72, 48);
+            batch.draw(blueTable, 328, 25);
             font.draw(batch,""+world.getScorePlayer1(),150, 580);
             font.draw(batch,""+world.getScorePlayer2(),1130, 580);
             for(int i = 0; i<swordX.length; i++){
-                batch.draw(blueBlock, swordX[i], swordY[i]);
+                batch.draw(blueBlock_s, swordX[i], swordY[i]);
             }
             for(int i = 0; i<defenseX.length; i++){
-                batch.draw(redBlock, defenseX[i], defenseY[i]);
+                batch.draw(redBlock_d, defenseX[i], defenseY[i]);
             }
             batch.end();
         }
         if(gameStage==3) {    
             batch.begin();
             batch.draw(bg, 0, 0);
-            batch.draw(redTable, 72, 48);
+            batch.draw(redTable, 328, 25);
             font.draw(batch,""+world.getScorePlayer1(),150, 580);
             font.draw(batch,""+world.getScorePlayer2(),1130, 580);
             for(int i = 0; i<swordX.length; i++){
-                batch.draw(redBlock, swordX[i], swordY[i]);
+                batch.draw(redBlock_s, swordX[i], swordY[i]);
             }
             for(int i = 0; i<defenseX.length; i++){
-                batch.draw(blueBlock, defenseX[i], defenseY[i]);
+                batch.draw(blueBlock_d, defenseX[i], defenseY[i]);
             }
             batch.end();
         }
